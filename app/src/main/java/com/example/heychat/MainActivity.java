c4d605c6ed2f1e7ad77553593b84d88042743e2b
unknown <2704muhammadahmed@gmail.com>
2025-05-10 21:06:49 +0500

    Logout + dialog box

--- app/src/main/java/com/example/heychat/MainActivity.java
+++ app/src/main/java/com/example/heychat/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.heychat;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +19,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgLogout;
 
     RecyclerView mainUserRecyclerView;
     UserAdapter adapter;
@@ -24,6 +31,38 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        imgLogout = findViewById(R.id.logoutMainImg);
+
+        imgLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(MainActivity.this, R.style.dialog);
+                dialog.setContentView(R.layout.dialog_layout);
+
+                Button no , yes;
+                yes = dialog.findViewById(R.id.dialog_button_Yes);
+                no = dialog.findViewById(R.id.dialog_button_No);
+
+                yes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, login.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+                no.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
+            }
+        });
+
         dbHelper = new DatabaseHelper(this);
         ArrayList<UserModel> userList = dbHelper.getAllUsers();
 
@@ -33,11 +72,5 @@ public class MainActivity extends AppCompatActivity {
         mainUserRecyclerView.setAdapter(adapter);
 
 
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
